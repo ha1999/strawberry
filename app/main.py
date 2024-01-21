91624c9063cd1d38cca99daa9fa3113ad7212f08
Lê Ngọc Hà (Haico) <43954353+ha1999@users.noreply.github.com>
2024-01-21 11:48:53 +0700

    :+1: Add root path (#1)
    Co-authored-by: haico <haico>

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 
+import datetime
+
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World"}
+
+
+@app.get("/ping")
+def serverInfo():
+    return {
+        "time": datetime.datetime.now(),
+        "info": {"name": "strawberry", "python": "3.9", "fastapi": "1.109"},
+    }
