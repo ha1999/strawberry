7471be8e6bfe7c6f37ea8ff8626519f6c06d412b
haico <haico>
2024-01-21 22:14:07 +0700

    :tada: Integration strawberry to app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import datetime
 from fastapi import FastAPI
 
-import datetime
+from app.graphql.main import graphql_app
 
 app = FastAPI()
 
@@ -16,3 +17,6 @@ def serverInfo():
         "time": datetime.datetime.now(),
         "info": {"name": "strawberry", "python": "3.9", "fastapi": "1.109"},
     }
+
+
+app.include_router(graphql_app, prefix="/graphql")
